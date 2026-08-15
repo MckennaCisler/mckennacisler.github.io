a307b03944752a043bdee3e084f6c3817c2d0f5c
Mckenna Cisler <mckennacisler@gmail.com>
2026-08-15 15:33:24 -0700

    update with Endurance
    and fix generate_html.bash bug with jsonresume infra changes

--- generate_html.bash
+++ generate_html.bash
@@ -2,7 +2,7 @@
 THEME_DIR=../../../../Coding/project/jsonresume-theme-concise/
 cp resume.json $THEME_DIR
 cd $THEME_DIR
-resume serve & # --theme=concise &
+resume serve --theme . & # --theme=concise &
 sleep 5
 pkill -f $(which resume)
 cd -
